92ca6cb92e430cfd102b212475716868678ca12a
Ketan S <Ketan S>
2017-07-22 19:17:52 -0400

    remove files

--- controller.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LIB_PATH=$"/home/yaoliu/src_code/local/libthrift-1.0.0.jar:/home/yaoliu/src_code/local/slf4j-log4j12-1.5.8.jar:/home/yaoliu/src_code/local/slf4j-api-1.5.8.jar:/home/yaoliu/src_code/local/log4j-1.2.14.jar"
-java -classpath bin/client_classes:$LIB_PATH Controller $1 $2
